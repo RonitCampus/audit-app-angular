9e8d44aaca48a85892b6fb1d47b52ad470d55fee
Rohan <rohaninfocepts@gmail.com>
2017-05-26 17:03:02 +0530

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuestionBankComponent } from './question-bank/question-bank.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuestionBankComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/question-bank', pathMatch: 'full' },
+      { path: 'question-bank', component: QuestionBankComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
