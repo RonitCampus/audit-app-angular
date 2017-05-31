d1f93e209895f6847d98a460068221ed0c6771a7
Rohan <rohaninfocepts@gmail.com>
2017-05-31 17:06:04 +0530

    31-05-17

---
+++ src/app/auditee-form/model/IQuestion.ts
@@ -0,0 +1,9 @@
+export interface IQuestion {
+  auditd: number;
+  projectId: number;
+  quetionId: number;
+  question: string;
+  weightage: number;
+  complianceStatus: number;
+  remark: string;
+}
